7f00d35b8532f17728ad728097757d384dc6a951
Arianna Ruggirello <ruggirelloarianna11@gmail.com>
2023-11-10 18:17:33 +0100

    add comments to home.js

--- controller/home.js
+++ controller/home.js
@@ -1,43 +1,44 @@
+// Importo i moduli necessari
 const express = require("express");
-
 const fs = require("fs");
 const path = require("path");
-// importo json
+
+// Importo i dati JSON da un file
 const posts = require('../db/posts.json');
 
-
 /**
- * @param {express.Request} req 
- * @param {express.Response} res 
+ * Gestisco la route dell'indice.
+ * @param {express.Request} req - L'oggetto di richiesta.
+ * @param {express.Response} res - L'oggetto di risposta.
  */
-function index(req,res){
+function index(req, res) {
+    // Gestisco diversi formati di risposta
     res.format({
-        'html' : ()=>{
+        'html': () => {
+            // Leggo il contenuto HTML dal file
             let htmlContent = fs.readFileSync(path.resolve(__dirname, "../index.html"), "utf-8");
-       
-            res.type('html').send(htmlContent)
+            res.type('html').send(htmlContent);
         }
-     
-    })
-
-   
+    });
 }
 
-
 /**
- * @param {express.Request} req 
- * @param {express.Response} res 
+ * Gestisco la route dei post.
+ * @param {express.Request} req - L'oggetto di richiesta.
+ * @param {express.Response} res - L'oggetto di risposta.
  */
-function post(req,res){
+function post(req, res) {
+    // Gestisco diversi formati di risposta
     res.format({
-        'html' : ()=>{
+        'html': () => {
+            // Creo una pagina HTML con i post
             const html = [`<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
           <h1>POST</h1>`];
     
-          html.push("<div class='row row-cols-3 justify-content-between '>");
+            html.push("<div class='row row-cols-3 justify-content-between '>");
     
-          for (const post of posts) {
-            html.push(`
+            for (const post of posts) {
+                html.push(`
             <div class='col'>
             <div class="card" style="width: 250px;">
             <img src="/imgs/${post.image}" class="card-img-top" alt="...">
@@ -47,31 +48,24 @@ function post(req,res){
             </div>
             </div>
             </div>`);
-          }
+            }
     
-          html.push("</div>");
+            html.push("</div>");
     
-          res.send(html.join(""));
-
-        // let postContent = fs.readFileSync(path.resolve(__dirname, "../post.html"), "utf-8");
-       
-        // res.type('html').send(postContent)
-
-           
+            res.send(html.join(""));
         },
-        'json' :()=>{
+        'json': () => {
+            // Rispondo con i dati dei post in formato JSON
             res.type('json').send({
                 totalElement: posts.length,
                 list: posts
-            })
+            });
         }
-    })
-    
+    });
 }
 
-
-// esporto
-module.exports={
+// Esporto le funzioni
+module.exports = {
     index,
     post
-}
+};
